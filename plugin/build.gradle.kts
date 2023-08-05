950af487b483dd703044f471383a14415cfa646a
Mugurell <Mugurell@users.noreply.github.com>
2023-08-05 20:35:48 +0300

    Update to latest gradle plugin and start signing this project's artifacts

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -3,11 +3,16 @@ plugins {
     kotlin("jvm") version "1.8.21"
     id("maven-publish")
     id("com.gradle.plugin-publish") version "1.1.0"
+    id("signing")
 }
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.0")
+}
+
+signing {
+    useGpgCmd()
 }
 
 gradlePlugin {
